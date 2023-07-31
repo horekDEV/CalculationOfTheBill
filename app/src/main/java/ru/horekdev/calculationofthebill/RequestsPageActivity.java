2a124479486810471fbb6dc4774bb57ab0e87800
horek <horektopksta@gmail.com>
2023-07-31 15:26:38 +0400

    optimize imports

--- app/src/main/java/ru/horekdev/calculationofthebill/RequestsPageActivity.java
+++ app/src/main/java/ru/horekdev/calculationofthebill/RequestsPageActivity.java
@@ -1,15 +1,14 @@
 package ru.horekdev.calculationofthebill;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class RequestsPageActivity extends AppCompatActivity {
     dataBaseManager dataBaseManager;
