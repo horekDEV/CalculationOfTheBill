68ea3d4249c1b4ea8bab3ca9a2501b36d162452b
horek <horektopksta@gmail.com>
2023-08-30 21:26:59 +0400

    change old code to new

--- app/src/main/java/ru/horekdev/calculationofthebill/MainActivity.java
+++ app/src/main/java/ru/horekdev/calculationofthebill/MainActivity.java
@@ -13,74 +13,70 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    EditText sum;
-    TextView title, twoPeople, threePeople, fourPeople, zero, twenty, fifty, hundred, order;
-    Button start;
-    Button clear;
-    ImageButton phone;
-    int percent;
-    int peopleCount;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sum = findViewById(R.id.sum);
+        EditText sum = findViewById(R.id.sum);
+        TextView title = findViewById(R.id.title);
 
-        title = findViewById(R.id.title);
+        TextView twoPeople = findViewById(R.id.twoPeople);
+        TextView threePeople = findViewById(R.id.threePeople);
+        TextView fourPeople = findViewById(R.id.fourPeople);
 
-        twoPeople = findViewById(R.id.twoPeople);
-        threePeople = findViewById(R.id.threePeople);
-        fourPeople = findViewById(R.id.fourPeople);
+        TextView zero = findViewById(R.id.zero);
+        TextView twenty = findViewById(R.id.twenty);
+        TextView fifty = findViewById(R.id.fifty);
+        TextView hundred = findViewById(R.id.hundred);
 
-        zero = findViewById(R.id.zero);
-        twenty = findViewById(R.id.twenty);
-        fifty = findViewById(R.id.fifty);
-        hundred = findViewById(R.id.hundred);
+        TextView order = findViewById(R.id.order);
 
-        order = findViewById(R.id.order);
+        Button start = findViewById(R.id.startClaculating);
+        Button clear = findViewById(R.id.clear);
+        ImageButton phone = findViewById(R.id.phone);
 
-        start = findViewById(R.id.startClaculating);
-        clear = findViewById(R.id.clear);
-        phone = findViewById(R.id.phone);
+        AtomicInteger percent = new AtomicInteger();
+        AtomicInteger peopleCount = new AtomicInteger(2);
 
         MediaPlayer click = MediaPlayer.create(this, R.raw.soundbutton);
 
 
         zero.setOnClickListener(view ->  {
-            percent = 0;
+            percent.set(0);
             Toast.makeText(MainActivity.this, "Вы выбрали, что чаевые будут составлять 0%", Toast.LENGTH_SHORT).show();
         });
 
         twenty.setOnClickListener(view ->  {
-            percent = 20;
+            percent.set(20);
             Toast.makeText(MainActivity.this, "Вы выбрали, что чаевые будут составлять 20%", Toast.LENGTH_SHORT).show();
         });
 
         fifty.setOnClickListener(view ->  {
-            percent = 50;
+            percent.set(50);
             Toast.makeText(MainActivity.this, "Вы выбрали, что чаевые будут составлять 50%", Toast.LENGTH_SHORT).show();
         });
 
         hundred.setOnClickListener(view ->  {
-            percent = 100;
+            percent.set(100);
             Toast.makeText(MainActivity.this, "Вы выбрали, что чаевые будут составлять 100%", Toast.LENGTH_SHORT).show();
         });
 
         twoPeople.setOnClickListener(view ->  {
-            peopleCount = 2;
+            peopleCount.set(2);
             Toast.makeText(MainActivity.this, "Вы выбрали, что людей которые будут делить деньги на заказ двое", Toast.LENGTH_SHORT).show();
         });
 
         threePeople.setOnClickListener(view ->  {
-            peopleCount = 3;
+            peopleCount.set(3);
             Toast.makeText(MainActivity.this, "Вы выбрали, что людей которые будут делить деньги на заказ трое", Toast.LENGTH_SHORT).show();
         });
 
         fourPeople.setOnClickListener(view ->  {
-            peopleCount = 4;
+            peopleCount.set(4);
             Toast.makeText(MainActivity.this, "Вы выбрали, что людей которые будут делить деньги на заказ четверо", Toast.LENGTH_SHORT).show();
         });
 
@@ -89,20 +85,20 @@ public class MainActivity extends AppCompatActivity {
             int result = 0;
             int tips;
 
-            if (peopleCount > 1) {
-                if (percent == 0) {
-                    result = Integer.parseInt(sum.getText().toString()) / peopleCount;
+            if (peopleCount.get() > 1) {
+                if (percent.get() == 0) {
+                    result = Integer.parseInt(sum.getText().toString()) / peopleCount.get();
                 } else {
-                    if (percent == 20) {
+                    if (percent.get() == 20) {
                         tips = (int) (Integer.parseInt(sum.getText().toString()) * 0.20);
-                        result = Integer.parseInt(sum.getText().toString()) + tips / peopleCount;
+                        result = Integer.parseInt(sum.getText().toString()) + tips / peopleCount.get();
 
-                    } else if (percent == 50) {
+                    } else if (percent.get() == 50) {
                         tips = (int) (Integer.parseInt(sum.getText().toString()) * 0.50);
-                        result = Integer.parseInt(sum.getText().toString()) + tips / peopleCount;
+                        result = Integer.parseInt(sum.getText().toString()) + tips / peopleCount.get();
 
-                    } else if (percent == 100) {
-                        result = (Integer.parseInt(sum.getText().toString()) + Integer.parseInt(sum.getText().toString())) / peopleCount;
+                    } else if (percent.get() == 100) {
+                        result = (Integer.parseInt(sum.getText().toString()) + Integer.parseInt(sum.getText().toString())) / peopleCount.get();
                     }
                 }
             } else {
@@ -125,8 +121,8 @@ public class MainActivity extends AppCompatActivity {
             order.setVisibility(View.INVISIBLE);
             title.setText("Счетчик");
 
-            peopleCount = 0;
-            percent = 0;
+            peopleCount.set(0);
+            percent.set(0);
         });
 
         phone.setOnClickListener(view -> {
