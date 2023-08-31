f13cbe2acbe934747bc57e71363fea3fd9484832
horek <horektopksta@gmail.com>
2023-08-31 16:24:26 +0400

    fix design color bug and add initial database logic

---
+++ app/src/main/java/ru/horekdev/calculationofthebill/dataBaseManager/DataBaseHelper.java
@@ -0,0 +1,41 @@
+package ru.horekdev.calculationofthebill.dataBaseManager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    Context context;
+    SQLiteDatabase.CursorFactory factory;
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public SQLiteDatabase.CursorFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(SQLiteDatabase.CursorFactory factory) {
+        this.factory = factory;
+    }
+
+    public DataBaseHelper(Context context, SQLiteDatabase.CursorFactory factory) {
+        super(context, "reports", factory, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE reports(id INT PRIMARY KEY, message TEXT, email VARCHAR(34))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS reports");
+        onCreate(db);
+    }
+}
