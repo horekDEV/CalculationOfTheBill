f3ab5c09c90c95ea7199f70813c85f35a6f38eb4
horekDEV <horektopksta@gmail.com>
2023-11-06 13:29:05 +0400

    optimize imports

--- app/src/main/java/ru/horekdev/calculationofthebill/MainActivity.java
+++ app/src/main/java/ru/horekdev/calculationofthebill/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.concurrent.atomic.AtomicInteger;
